08fef573400c31314c21ba7bbfe2d2ab5b0031d0
Jochen Schalanda <jochen@graylog.com>
2016-04-27 14:26:49 +0200

    Update MQTTInputMetadata

--- src/main/java/org/graylog2/inputs/mqtt/MQTTInputMetadata.java
+++ src/main/java/org/graylog2/inputs/mqtt/MQTTInputMetadata.java
@@ -5,7 +5,7 @@ import org.graylog2.plugin.ServerStatus;
 import org.graylog2.plugin.Version;
 
 import java.net.URI;
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Set;
 
 public class MQTTInputMetadata implements PluginMetaData {
@@ -21,17 +21,17 @@ public class MQTTInputMetadata implements PluginMetaData {
 
     @Override
     public String getAuthor() {
-        return "TORCH GmbH";
+        return "Graylog, Inc.";
     }
 
     @Override
     public URI getURL() {
-        return URI.create("http://www.torch.sh");
+        return URI.create("https://www.graylog.org");
     }
 
     @Override
     public Version getVersion() {
-        return new Version(1, 1, 2);
+        return new Version(2, 0, 0);
     }
 
     @Override
@@ -41,11 +41,11 @@ public class MQTTInputMetadata implements PluginMetaData {
 
     @Override
     public Version getRequiredVersion() {
-        return new Version(1, 0, 0);
+        return new Version(2, 0, 0);
     }
 
     @Override
     public Set<ServerStatus.Capability> getRequiredCapabilities() {
-        return new HashSet<>();
+        return Collections.emptySet();
     }
 }
